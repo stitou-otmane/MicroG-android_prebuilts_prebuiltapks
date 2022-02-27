ae2a1d85e7bb2fa305de08ee6f38dc7e47156a56
Otmane <stitou.otmane@gmail.com>
2022-02-27 15:56:47 +0100

    Keep only Gsf

--- microg.mk
+++ microg.mk
@@ -1,17 +1,5 @@
 # Product priv-app
 PRODUCT_PACKAGES += \
-    additional_repos \
-    AuroraServices \
-    AuroraStore \
-    com.google.android.maps.xml \
-    com.google.android.maps.jar \
-    FakeStore \
-    FDroid \
-    FDroidPrivilegedExtension \
-    GmsCore \
     GsfProxy \
-    microg-a5k \
-    MozillaNlpBackend \
-    NominatimNlpBackend \
-    OpenWeatherMapWeatherProvider
+    microg-a5k
 
